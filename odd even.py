3eb5cc4a04f3bf21d860452e346828cd3836e134
bidan <bidhan.adhikari@myy.haaga-helia.fi>
2024-05-22 11:53:36 +0300

    odd even test

---
+++ odd even.py
@@ -0,0 +1,8 @@
+# Ask the user for a number
+number = int(input("Please enter a number: "))
+
+# Check if the number is even
+if number % 2 == 0:
+    print("The given number was even.")
+else:
+    print("The given number was odd.")
